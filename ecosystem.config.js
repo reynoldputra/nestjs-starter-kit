900f9b0f020fb9d966184752f86126a34e8e7763
reynoldputra <reynoldputra1@gmail.com>
2024-02-24 22:40:51 +0700

    fix: change trigger workflow for production

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,16 @@
 module.exports = {
-    apps: [
-        {
-            name: 'eticket-backend',
-            exec_mode: 'cluster',
-            instances: 'max',
-            script: 'dist/main.js',
-            env_production: {
-                ENVIRONMENT: 'production',
-            },
-            env_development: {
-                ENVIRONMENT: 'development',
-            },
-        },
-    ],
+  apps: [
+    {
+      name: 'backend',
+      exec_mode: 'cluster',
+      instances: 'max',
+      script: 'dist/main.js',
+      env_production: {
+        ENVIRONMENT: 'production',
+      },
+      env_development: {
+        ENVIRONMENT: 'development',
+      },
+    },
+  ],
 };
